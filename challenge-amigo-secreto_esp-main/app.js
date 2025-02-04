84f3bcb5aeffa7ee9daf918089e78c5c2d380c1c
HypeJF <josemanuellopera1@gmail.com>
2025-02-03 21:21:23 -0500

    Añade funcion agregarAmigo

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,4 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 
-//Test de Branch
+//3-2-25
+
+let participantes = [];
+
+function agregarAmigo() {
+    var nombre = document.getElementById("amigo").value;
+    if (nombre == "") {
+        alert("Por favor, ingrese un nombre.");
+        return;
+    } else {
+        participantes.push(nombre);
+        console.log(participantes);
+        document.getElementById("amigo").value = "";
+    }
+    
+}
