f0248f6e86699e4803518a5f50c77e92012a195b
HypeJF <josemanuellopera1@gmail.com>
2025-02-04 16:35:20 -0500

    Se añade funcion de lista de amigos y un event listener para enter

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -2,6 +2,13 @@
 
 
 //3-2-25
+//Puse un event listener para ahorrar tiempo en el input
+window.addEventListener("keydown",(e)=> {
+    if (e.keyCode === 13) {
+        agregarAmigo();
+    }
+})
+
 
 let participantes = [];
 
@@ -15,5 +22,25 @@ function agregarAmigo() {
         console.log(participantes);
         document.getElementById("amigo").value = "";
     }
-    
+    mostrarListaAmigos(participantes);
 }
+
+function mostrarListaAmigos(lista) {
+    for (let i = 0; i < lista.length; i++) {
+        var list = document.getElementById("listaAmigos");
+        var new_li = document.createElement("li");
+        new_li.innerHTML = lista[i];
+        list.appendChild(new_li);
+        lista[i] = "";
+} }
+
+function sortearAmigo() {
+    document.getElementById("textolista").innerHTML='HolaTexto'
+    var list = document.getElementById("listaAmigos");
+    var new_li = document.createElement("li");
+    new_li.innerHTML = "Texto";
+    list.appendChild(new_li);
+
+}
+
+//crear for para recorrer el array de participantes
